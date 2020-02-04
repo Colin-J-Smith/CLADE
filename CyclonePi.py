db2bd39511e25c04ac2491388f8de163355e8474
colin <cosm5801@colorado.edu>
2020-02-04 09:10:00 -0700

    first commit

---
+++ CyclonePi.py
@@ -0,0 +1,106 @@
+
+Colin Smith <Colin.Smith@colorado.edu>
+Mon, Feb 3, 7:18 PM (13 hours ago)
+to me
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 18:12:57 2020
+
+@author: colin
+"""
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+# INIT MODE
+# Set up 7 of the pins to control the LEDS
+# Set the cycle period
+
+# PAUSE MODE
+# Turn off all LEDS
+# Reset the cycle timer
+# Wait for a button press to start the cycle timer
+# Set the incremement to the first LED
+
+# PLAY MODE
+# Listen for a button press (with debounce)
+## If button pressed, check which LED is active
+### If green LED is active, keep the green LED on for 3 seconds and switch to PAUSE
+### If another LED is pressed, continue PLAY mode
+
+
+class Cyclone(object):
+   
+    def __init__(self, period=0.5):
+        self.last_time = time.perf_counter()
+        self.led_pins = range(7)
+        self.leds = {}
+       
+        self.button_pin = 10
+        self.last_button_state = False
+       
+        self.winning_led = 3 # uses a 0 indexed list
+        self.current_led = 0
+       
+        self.period = 1 # number of seconds each led is lit up
+       
+        # Set up the GPIO LED pins
+        for channel in self.led_pins:
+            self.leds[str(channel)] = GPIO.PWM(channel, 50) # PWM at 50Hz
+       
+        # Set up the button input pin
+        GPIO.setup(self.button_pin, GPIO.IN)
+       
+        # Enter pause mode
+        self.pause()
+   
+    def pause(self):
+        # turn off all leds
+        for i, val in enumerate(self.leds):
+            val.stop()
+       
+        # wait for the button to be pressed before restarting
+        while GPIO.input(self.button_pin):
+            time.sleep(0.1)
+        self.play()
+       
+   
+    def play(self):
+        # start a single period
+        while self.last_time - time.perf_counter() < self.period:
+            # Light the next LED
+            self.leds[str(self.current_led)].start(50) # 50% duty cycle
+           
+            # check the button state
+            button_state = GPIO.input(self.button_pin)
+           
+            # If the button has been pressed only during this iteration
+            if (self.last_button_state != button_state) and (button_state):
+                # check if the active led pin is a winner
+                if self.current_led == self.winning_led:
+                    time.sleep(3) # wait for 3 seconds
+                    self.pause() # pause the game
+                    return
+                   
+            # save the button state for this check
+            self.last_button_state = button_state
+                       
+            # increment the LED pin
+            if self.current_led == 6:
+                self.current_led = 0
+            else:
+                self.current_led += 1
+         
+        # When a single period finishes
+        # turn off the led
+        try:
+            self.leds[str(self.current_led-1)].stop()
+        except:
+            pass
+        # record the iteration time
+        self.last_time = time.perf_counter()
+        # repeat the loop
+        self.play() 
