f3ef959633e205cd2164378b8d579db6e99bc0dc
Lea Chandler <lech4708@colorado.edu>
2020-04-15 16:22:30 -0700

    debugging

--- insta_shoot.py
+++ insta_shoot.py
@@ -4,8 +4,23 @@ import time
 import serial
 from datetime import datetime
 
+# commands
+fire    = "<FIR>"
+stop    = "<STP>" # send STP when target is found
+up      = "<UPP>"
+down    = "<DWN>"
+left    = "<LFT>"
+right   = "<RGT>"
+home    = "<HOM>" # send HOM when target has been hit/no bad target in sight
+
+
 arduino_port_tur = "/dev/ttyACM0"
 arduino_baudrate_tur = 9600
 
 tur_write = serial.Serial(port=arduino_port_tur, baudrate=arduino_baudrate_tur)
-tur_write.write("<FIR>".encode('utf-8'))
+tur_write.write("right".encode('utf-8'))
+tur_write.write("right".encode('utf-8'))
+tur_write.write("right".encode('utf-8'))
+tur_write.write("left".encode('utf-8'))
+tur_write.write("left".encode('utf-8'))
+tur_write.write("left".encode('utf-8'))
