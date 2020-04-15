3e29461712406d31e4d3b8bcac40aba0324d1d9b
Lea Chandler <lech4708@colorado.edu>
2020-04-15 15:55:03 -0700

    debugging

--- targeting.py
+++ targeting.py
@@ -72,13 +72,7 @@ def target(target_write_input):
                 data1 = data[1,:,:]
                 data2 = data[2,:,:]
                 frame_bgr = cv2.merge([data0, data1, data2])
-                print(len(frame_bgr[0]))
-                print(len(frame_bgr[1]))
-                print(len(frame_bgr[2]))
-                print(len(frame_bgr))
-                print(frame_bgr)
                 cv2.imshow(packet.stream_name,frame_bgr)
-                return
                 #processed_frame = process_image(frame_bgr[2])
                 #cv2.imshow(packet.stream_name, processed_frame)
 
