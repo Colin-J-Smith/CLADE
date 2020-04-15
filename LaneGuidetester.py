51717efc657841f67097a9a1ec2e178fa04cf826
dan <dawa1272@colorado.edu>
2020-04-15 11:54:17 -0600

    tester HSV values purple

--- LaneGuidetester.py
+++ LaneGuidetester.py
@@ -358,7 +358,7 @@ def process_intersection(frame, intersection_vertices):
     gray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
     processed_hsv = cv2.cvtColor(ROI, cv2.COLOR_BGR2HSV)
     lower_purple = np.array([140, 35, 120], dtype=int)
-    upper_purple = np.array([170, 255, 200], dtype=int)
+    upper_purple = np.array([170, 255, 210], dtype=int)
     mask_purple = cv2.inRange(processed_hsv, lower_purple, upper_purple)
     processed = cv2.bitwise_and(gray, mask_purple)
 
