e6bb2adb46979cbb5b30445e7aac898927226592
Lea Chandler <lech4708@colorado.edu>
2020-04-15 16:15:28 -0700

    debugging

---
+++ insta_shoot.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import time
+import serial
+from datetime import datetime
+
+arduino_port_tur = "/dev/ttyACM0"
+arduino_baudrate_tur = 9600
+
+tur_write = serial.Serial(port=arduino_port_tur, baudrate=arduino_baudrate_tur)
+tur_write.write("<FIR>")
