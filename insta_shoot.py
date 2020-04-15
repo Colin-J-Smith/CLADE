027f4a4c33cb879eed6dabe2213a56567c8483fb
Lea Chandler <lech4708@colorado.edu>
2020-04-15 16:19:48 -0700

    debugging

--- insta_shoot.py
+++ insta_shoot.py
@@ -8,6 +8,4 @@ arduino_port_tur = "/dev/ttyACM0"
 arduino_baudrate_tur = 9600
 
 tur_write = serial.Serial(port=arduino_port_tur, baudrate=arduino_baudrate_tur)
-tur_write.write("<RGT>".encode('utf-8'))
-tur_write.write("<RGT>".encode('utf-8'))
-tur_write.write("<RGT>".encode('utf-8'))
+tur_write.write("<FIR>".encode('utf-8'))
