fd230b4dd8b21485dc9678513017fa7c650d18d9
Lea Chandler <lech4708@colorado.edu>
2020-04-22 12:27:14 -0700

    testing turret up/down comms

---
+++ test_up_down.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+import serial
+from datetime import datetime
+
+# commands
+fire    = "<FIR>"
+stop    = "<STP>" # send STP when target is found
+up      = "<UPP>"
+down    = "<DWN>"
+left    = "<LFT>"
+right   = "<RGT>"
+home    = "<HOM>" # send HOM when target has been hit/no bad target in sight
+
+
+arduino_port_tur = "/dev/ttyACM0"
+arduino_baudrate_tur = 9600
+
+#tur_write = serial.Serial(port=arduino_port_tur, baudrate=arduino_baudrate_tur)
+tur_write = sys.stdout
+
+
+t_end = time.time() + 2
+while time.time() < t_end:
+    tur_write.write(up.encode('utf-8'))
+
+t_end = time.time() + 2
+while time.time() < t_end:
+    tur_write.write(down.encode('utf-8'))
