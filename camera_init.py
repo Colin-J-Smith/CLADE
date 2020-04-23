d38fea27de4162d4b10acd075866e81889b031ff
Lea Chandler <lech4708@colorado.edu>
2020-04-23 14:38:34 -0700

    changed luxonis fps

--- camera_init.py
+++ camera_init.py
@@ -12,6 +12,7 @@
 
 import luxonis_resources.depthai as depthai
 import os
+import json
 from os import path
 from pathlib import Path
 
@@ -32,31 +33,23 @@ blob_file          = relative_to_abs_path('luxonis_resources/mobilenet_ssd.blob'
 # INIT
 # --------------------------
 
-def camera_init(streams=None, configs=None):
-
-    # output streams: left, right, previewout, metaout, disparity, depth_sipp, depth_color_h
-    if streams is None:
-        #streams = ['left', 'right', 'previewout'] # make sure to always put 'left' first
-        streams = [{'name': 'depth_sipp', "max_fps": 12.0}, {'name': 'previewout', "max_fps": 12.0}, ]
- 
-    # camera configs
-    if configs is None:
-        configs = {
-            'streams': streams,
-            'depth': {
-                'calibration_file': '', # we're going to do our own calibration since we're not using their AI
-                'padding_factor': 0.3
-            },
-            'ai': { 
-                'blob_file': blob_file,
-                'calc_dist_to_bb': False                # I think this will decrease processing time
-            },
-            'board_config': {
-                'swap_left_and_right_cameras': True,    # True for 1097 (RPi Compute) and 1098OBC (USB w/onboard cameras)
-                'left_fov_deg': 69.0,                   # Same on 1097 and 1098OBC
-                'left_to_right_distance_cm': 7.5,       # Distance between stereo cameras
-            }
+def camera_init():
+    configs = {
+        'streams': [{'name': 'previewout', "max_fps": 12.0},],
+        'depth': {
+            'calibration_file': '', # we're going to do our own calibration since we're not using their AI
+            'padding_factor': 0.3
+        },
+        'ai': { 
+            'blob_file': blob_file,
+            'calc_dist_to_bb': False                # I think this will decrease processing time
+        },
+        'board_config': {
+            'swap_left_and_right_cameras': True,    # True for 1097 (RPi Compute) and 1098OBC (USB w/onboard cameras)
+            'left_fov_deg': 69.0,                   # Same on 1097 and 1098OBC
+            'left_to_right_distance_cm': 7.5,       # Distance between stereo cameras
         }
+    }
 
     # camera init
     if not depthai.init_device(device_cmd_file):
