3deaa28d895dc9173f50eeb6c32985dbe4083084
dan <dawa1272@colorado.edu>
2020-04-24 12:36:31 -0600

    adjust guidance decision logic

--- LaneGuidev10.py
+++ LaneGuidev10.py
@@ -349,10 +349,10 @@ def intersection_roi(frame):
     """ Define Region of Interest (ROI) for the intersection """
     # currently the full screen, but can be adjusted to filter further
     rows, cols = frame.shape[:2]
-    bottom_left = [cols * 0.0, rows * 1]
-    top_left = [cols * 0.0, rows * 0.0]
-    bottom_right = [cols * 1, rows * 1]
-    top_right = [cols * 1, rows * 0.0]
+    bottom_left = [cols * 0.1, rows * 1]
+    top_left = [cols * 0.1, rows * 0.2]
+    bottom_right = [cols * 0.9, rows * 0.9]
+    top_right = [cols * 0.9, rows * 0.2]
     intersection_vertices = np.array([[bottom_left, top_left, top_right, bottom_right]], dtype=np.int32)
     return intersection_vertices
 
